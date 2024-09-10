434175d777d70101caf4cfcb971fa96f8127d81d
Anila Anumolu <Anila.Anumolu@philips.com>
2024-09-10 09:53:41 -0700

    separation of main

---
+++ ColorPair.h
@@ -0,0 +1,26 @@
+namespace TelCoColorCoder
+{
+	enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+	
+	const char* MajorColorNames[];
+	const char* MinorColorNames[];
+	
+	int numberOfMajorColors;
+	int numberOfMinorColors;
+	
+	class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor);
+            MajorColor getMajor();
+            MinorColor getMinor(); 
+            std::string ToString();
+    };
+	
+	ColorPair GetColorFromPairNumber(int pairNumber);
+	int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+	std::string ColorPairReferenceManual();
+}
